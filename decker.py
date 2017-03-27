f7d3facfd5729ea5c54ea9d5eae19993ac96be10
hel <h.toedtmann@ifaoe.de>
2017-03-27 15:59:54 +0200

    working changes

--- decker.py
+++ decker.py
@@ -14,50 +14,46 @@ from PIL import Image as PILImage
 
 customsize = (6.9 * cm, 9.4 * cm)
 
-# doc = SimpleDocTemplate("/home/hel/hello.pdf",pagesize=customsize,
-#                         rightMargin=0,leftMargin=0,
-#                         topMargin=0.085*cm+0.35/2*cm,bottomMargin=0)
-
 doc = SimpleDocTemplate("/home/hel/hello.pdf", pagesize=customsize,
                         rightMargin=0, leftMargin=0,
                         topMargin=0.1 * cm, bottomMargin=0)
 Story = []
 
+early_stop_at = 1
+
 decklist = '/home/hel/deckerdata/UR steal hate.cod'
 
 e = xml.etree.ElementTree.parse(decklist).getroot()
 cardnamelist = []
-cardnumberslist = []
+
+
+childcount = 0
 
 for child in e:
-    print(child.tag, child.attrib)
+    # print(child.tag, child.attrib)
     if child.tag == 'zone':
         if child.attrib['name'] == 'main':
-            print('here')
+            # numberofcards = len(child.getchildren())
+            # print('numberofcards',numberofcards)
             for subchild in child:
+                childcount += 1
+                if childcount > early_stop_at:
+                    continue
+
                 print(subchild.tag, subchild.attrib)
-                cardnamelist.append(subchild.attrib['name'])
-                cardnumberslist.append(subchild.attrib['number'])
-
-
-# cardname = cardnamelist[0]
-
-# query_adress = "http://magiccards.info/query?q=" + cardname.replace(' ', '+') + "&v=card&s=cname"
+                for i in range(0,int(subchild.attrib['number'])):
+                    cardnamelist.append(subchild.attrib['name'])
 
 QUERIES = []
 count = len(cardnamelist)
 
 for c in range(0, count):
-
     query_adress = "http://magiccards.info/query?q=" + cardnamelist[c].replace(' ', '+') + "&v=card&s=cname"
-    QUERIES.append([query_adress, 1])
+    QUERIES.append(query_adress)
 
 print (QUERIES)
 
 def get_content(url):
-
-    url = url[0]
-    number = url[1]
 
     url = urllib.request.urlopen(url)
     s = url.read()
@@ -66,15 +62,14 @@ def get_content(url):
     pos1 = str.find(content, targetpattern)
     pos2 = str.find(content, '"', pos1)
     img_adress = content[pos1:pos2]
-    print(img_adress)
+    print("downloading :",img_adress)
 
     img = urllib.request.urlopen(img_adress)
     s = img.read()
 
     return s
 
-
-pool = multiprocessing.Pool(processes=128)  # play with ``processes`` for best results
+pool = multiprocessing.Pool(processes=256)  # play with ``processes`` for best results
 results = pool.map(get_content, QUERIES)  # This line blocks, look at map_async
 # for non-blocking map() call
 pool.close()  # the process pool no longer accepts new tasks
@@ -86,42 +81,35 @@ resultlength = len(results)
 
 for r in range(0,resultlength):
 
-    if(r>=55):
-        continue
-
     targetfilename = '/home/hel/deckerdata/'+cardnamelist[r]+'.jpg'
     f = open(targetfilename, 'wb')
     f.write(results[r])
     f.close()
     im2 = PILImage.open(targetfilename)  # 312 * 445
     # print(im2.size)
-    if im2.size != (312,445):
+
+    fakeblackwidth = 0
+
+    width, height = im2.size
+
+    im2 = im2.crop((fakeblackwidth,fakeblackwidth,width-fakeblackwidth,height-fakeblackwidth))
+
+
+    if im2.size != (312, 445):
         print(im2.size)
         print('CARD SIZE WARNING!')
-    custom_offset = 15
-    im3 = PILImage.new("RGB", (312 + custom_offset * 2, 445 + custom_offset * 2), "black")
+    custom_offset = 15+fakeblackwidth*2
+
+
+    im3 = PILImage.new("RGB", (312 + custom_offset * 2, 445 + custom_offset * 2), "red")
     im3.paste(im2, (custom_offset, custom_offset))
     tempname = "/home/hel/deckerdata/"+cardnamelist[r]+".temp.png"
     im3.save(tempname, 'PNG')
     im = Image(tempname, 6.3 * cm, 8.8 * cm, hAlign='CENTER')
 
-
-    for i in cardnumberslist[r]:
-        Story.append(im)
-        Story.append(pb)
-
-
+    Story.append(im)
+    print('appending :', tempname)
+    Story.append(pb)
 
 doc.build(Story)
 
-# DUMP
-
-# for x in range(0, 100):
-# url = urllib.request.urlopen(adress)
-# s = url.read()
-# f = open('/home/hel/deckerdata/00000001.jpg','wb')
-# f.write(s)
-# f.close()
-# print(x)
-
-# filename = wget.download(adress)
